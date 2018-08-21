3950510c519085a07fed5255d611136389f1b4a6
Abram Van Der Geest <avanderg@tibco.com>
2018-08-21 13:58:49 -0400

    start activity to read data prep json

---
+++ activity/dataprep/activity.go
@@ -0,0 +1,41 @@
+package dataprep
+
+import (
+	"os"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-tibco-dataprep")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// do eval
+
+	jsonFile, err := os.Open("dataprep.json")
+	if err != nil {
+		log.Errorf("Cannot read dataprep.json")
+	}
+
+	defer jsonFile.Close()
+
+	return true, nil
+}
