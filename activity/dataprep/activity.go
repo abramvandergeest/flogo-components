f87bbd987dfac302c8eca8b457e728f763ea766d
Abram Van Der Geest <avanderg@tibco.com>
2018-08-21 14:55:01 -0400

    saving progress for dataprep activity

--- activity/dataprep/activity.go
+++ activity/dataprep/activity.go
@@ -1,7 +1,12 @@
 package dataprep
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
+
+	// "json"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
@@ -9,6 +14,32 @@ import (
 
 // log is the default package logger
 var log = logger.GetLogger("activity-tibco-dataprep")
+
+// JSONObject primary structure of dataprep json
+type JSONObject struct {
+	InObj  InputObject       `json:"input"`
+	OpObjs []OperationObject `json:"operations"`
+	OutObj OutputObject      `json:"output"`
+}
+
+// InputObject is the sub-Json that handles input structure
+type InputObject struct {
+	Type  string `json:"type"`
+	Shape string `json:"shape"`
+}
+
+// OperationObject is a struct of operations - to be developed further
+type OperationObject struct {
+	Name string `json:"name"`
+	List string `json:"list"`
+}
+
+// OutputObject is the sub-Json that handles input structure
+type OutputObject struct {
+	Type  string `json:"type"`
+	Shape string `json:"shape"`
+	Build string `json:"build"`
+}
 
 // MyActivity is a stub for your Activity implementation
 type MyActivity struct {
@@ -28,14 +59,23 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
-	// do eval
+	filename := "dataprep.json"
 
-	jsonFile, err := os.Open("dataprep.json")
+	file, err := os.Open(filename)
 	if err != nil {
-		log.Errorf("Cannot read dataprep.json")
+		log.Errorf("Can't open file %s", filename)
+		return false, err
+	}
+	defer file.Close()
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Errorf("Cannot read dataprep.json into bytes")
 	}
 
-	defer jsonFile.Close()
+	var jsontype JSONObject
+	json.Unmarshal(byteValue, &jsontype)
+	fmt.Println(jsontype)
 
 	return true, nil
 }
